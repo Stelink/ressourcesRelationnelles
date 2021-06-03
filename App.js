391794b2eca744a7aad9e05e523647904892a979
PerrierH <hugues14500@gmail.com>
2021-06-03 10:19:38 +0200

    Navigation bar

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Inscription from "./pages/Inscription";
+import Connexion from "./pages/Connexion";
+import Chargement from "./pages/Chargement";
+import Navigation from "./components/Navigation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      Navigation()
   );
 }
 
--- pages/Chargement.js
+++ pages/Chargement.js
@@ -3,7 +3,7 @@ import {StyleSheet, Text, View, Image} from 'react-native';
 import marianne from '../assets/marianne.jpg';
 import logo from '../assets/logo.png';
 
-export default function Chargement() {
+export default function Chargement({navigation}) {
 
     return (
         <View style={styles.container}>
