950d57a544db710fb1bd940ca5fee5cfb874a7ba
PerrierH <hugues14500@gmail.com>
2021-06-03 11:08:27 +0200

    incription css

--- pages/Inscription.js
+++ pages/Inscription.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, Image, StyleSheet, Text, TextInput, View} from 'react-native';
+import user from "../assets/user.png";
 
 export default function Inscription({navigation}) {
 
     return (
         <View style={styles.container}>
-            <Text h1>Inscription</Text>
+            <Image source={user} style={{ width: 150, height: 150, marginTop: 25}} />
 
-            <Text>Nom</Text>
-            <TextInput style={styles.inputX } placeholder="Nom"/>
+            <View style={styles.nomPrenom}>
 
-            <Text>Prenom</Text>
-            <TextInput style={styles.inputX } placeholder="Prenom"/>
+                <View style={{marginRight: 50}}>
+                    <Text>Nom</Text>
+                    <TextInput style={styles.inputX } placeholder="     Nom"/>
+                </View>
 
+                <View>
+                    <Text>Prenom</Text>
+                    <TextInput style={styles.inputX } placeholder="     Prenom"/>
+                </View>
+
+            </View>
             <Text>Email</Text>
-            <TextInput style={styles.input} placeholder="Email"/>
+            <TextInput style={styles.input} placeholder="     Email"/>
 
             <Text>Mot de passe</Text>
-            <TextInput style={styles.input } placeholder="***"/>
+            <TextInput style={styles.input } placeholder="     ***"/>
 
             <Text>Répéter votre mot de passe</Text>
-            <TextInput style={styles.input } placeholder="***"/>
+            <TextInput style={styles.input } placeholder="     ***"/>
 
             <Button title={"Connexion"}/>
         </View>
@@ -34,10 +42,11 @@ const styles = StyleSheet.create({
     },
     input: {
         height: 40,
-        width: 120,
+        width: 350,
         borderColor: 'black',
         borderWidth: 2,
         borderRadius: 20,
+        marginBottom: 10,
     },
     inputX: {
         display: 'flex',
@@ -47,6 +56,14 @@ const styles = StyleSheet.create({
         borderColor: 'black',
         borderWidth: 2,
         borderRadius: 20,
+        marginBottom: 10,
+    },
+    nomPrenom: {
+        flex: 1,
+        flexDirection: 'row',
+        marginTop: 50,
+        marginBottom: 75,
+        justifyContent: 'space-between',
     }
 });
 
